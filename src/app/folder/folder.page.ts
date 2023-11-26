3b3b584dc2e0c58ed9d33f4a30ad73a14f071dd0
EngiBeerHub <engibeer0422@gmail.com>
2023-11-26 11:11:39 +0900

    CRUD

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -7,11 +7,29 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
+  title = 'タスク登録';
+  tasks: { name: string }[] = [{ name: 'タスク1' }, { name: 'タスク2' }];
+  task!: string;
+
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
   constructor() {}
 
+  ionViewWillEnter() {
+    if ('tasks' in localStorage) {
+      this.tasks = JSON.parse(localStorage['tasks']);
+    }
+  }
+
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+  }
+
+  addTask() {
+    this.tasks.push({
+      name: this.task,
+    });
+    localStorage['tasks'] = JSON.stringify(this.tasks);
+    this.task = '';
   }
 }
